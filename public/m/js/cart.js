422738a26e53f69358dd96ca1f651a549007e96d
ximorubing <ximorubing@sina.com>
2019-11-08 07:33:26 +0000

    add cart.html

--- public/m/js/cart.js
+++ public/m/js/cart.js
@@ -1,8 +1,16 @@
 $(function(){
+	var result=null;
+	var changeSize=null;
+	var num=null;
+	var productNum=null;
 	$.ajax({
 		url:'/cart/queryCart',
 		success:function(res){
 			
+			if(res.error==400){
+				location.href='login.html';
+			}
+			result=res;
 			var html=template("cartTp1",{result:res});
 			
 			$("#cartList").html(html);
@@ -33,16 +41,54 @@ $(function(){
 			}
 		})
 	})
+	$("body").on("tap","#changeSize span",function(){
+		$(this).addClass("active").siblings().removeClass("active");
+		changeSize=$(this).html();
+	})
+	$("body").on("tap",".decrease",function(){
+		num=Number($("input[name='num']").val());
+		num--;
+		
+		if(num<1){
+			num=1;
+		}
+		$("input[name='num']").val(num);
+		
+	})
+	$("body").on("tap",".increase",function(){
+		num=Number($("input[name='num']").val());
+		num++;
+		if(num>productNum){
+			num=productNum;
+		}
+		$("input[name='num']").val(num);
+	})
 	$("#cartList").on("tap",".edit-btn",function(){
-		var that=$(this);
-		mui.confirm('是否删除','提示',['确定','取消'],function (e) {
+		var id=$(this).attr("dataId");
+		var data=null;
+		for(var i=0;i<result.length;i++){
+			if(id==result[i].id){
+				data=result[i];
+				changeSize=data.size;
+				num=data.num;
+				productNum=data.productNum;
+			}
+		}
+		
+		var html=template("editTp1",data);
+		
+		mui.confirm(html,'编辑商品',['确定','取消'],function (e) {
 			
 			if(e.index==0){
-				var id=that.attr("dataId");
+				
 				$.ajax({
-					url:'/cart/deleteCart',
+					url:'/cart/updateCart',
+					type:'post',
 					data:{
-						id:id},
+						id:id,
+						size:changeSize,
+						num:num
+						},
 					success:function(res){
 						if(res.success){
 							location.href="cart.html";
